1f60297d7e67d3a0bd67668eb96c89113ec60da0
ken44282 <knagai428@gmail.com>
2022-06-16 00:10:30 +0900

    add image function

--- src/pdfshield/core.clj
+++ src/pdfshield/core.clj
@@ -1,17 +1,18 @@
 (ns pdfshield.core
   (:import [java.io ByteArrayOutputStream File FileInputStream FileOutputStream]
+           [org.apache.fontbox.ttf TrueTypeCollection]
+           [org.apache.pdfbox.pdmodel.graphics.image PDImageXObject]
            [org.apache.pdfbox.pdmodel PDDocument PDPageContentStream PDPageContentStream$AppendMode]
-           [org.apache.pdfbox.pdmodel.font PDType0Font]
-           [org.apache.fontbox.ttf TrueTypeCollection])
+           [org.apache.pdfbox.pdmodel.font PDType0Font])
   (:gen-class))
 
 (defn set-text [contents page-num size x y text]
   (let [document (PDDocument/load contents)
         page (.getPage document page-num)
-        bos (ByteArrayOutputStream.)
         ttc (TrueTypeCollection. (File. "resources/msgothic.ttc"))
         ttf (.getFontByName ttc "MS-Gothic")
-        font (PDType0Font/load document ttf true)]
+        font (PDType0Font/load document ttf true)
+        bos (ByteArrayOutputStream.)]
     (doto (PDPageContentStream. document page PDPageContentStream$AppendMode/APPEND true true)
       (.beginText)
       (.setFont font size)
@@ -29,6 +30,18 @@
     (doto (FileOutputStream. "resources/test2.pdf")
       (.write byte-arr)
       (.close))))
+
+(defn set-image [contents img-byte page-num x y, width, height]
+  (let [document (PDDocument/load contents)
+        page (.getPage document page-num)
+        img (PDImageXObject/createFromByteArray document img-byte "img")
+        bos (ByteArrayOutputStream.)]
+    (doto (PDPageContentStream. document page PDPageContentStream$AppendMode/APPEND true true)
+      (.drawImage img x y width height)
+      (.close))
+    (.save document bos)
+    (.close document)
+    (.toByteArray bos)))
 
 (defn -main
   "I don't do a whole lot ... yet."
