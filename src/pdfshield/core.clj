500ff28019759d3927f639a54f0b1d4b913012ef
ken44282 <knagai428@gmail.com>
2022-06-17 00:53:46 +0900

    add test-set-image

--- src/pdfshield/core.clj
+++ src/pdfshield/core.clj
@@ -31,9 +31,10 @@
       (.write byte-arr)
       (.close))))
 
-(defn set-image [contents img-byte page-num x y, width, height]
+(defn set-image [contents img-ins page-num x y width height]
   (let [document (PDDocument/load contents)
         page (.getPage document page-num)
+        img-byte (.readAllBytes img-ins)
         img (PDImageXObject/createFromByteArray document img-byte "img")
         bos (ByteArrayOutputStream.)]
     (doto (PDPageContentStream. document page PDPageContentStream$AppendMode/APPEND true true)
@@ -42,6 +43,14 @@
     (.save document bos)
     (.close document)
     (.toByteArray bos)))
+
+(defn test-set-image []
+  (let [fis (FileInputStream. "resources/test.pdf")
+        img-ins (FileInputStream. "resources/test.png")
+        byte-arr (set-image fis img-ins 1 (float 20) (float 30) (float 100) (float 200))]
+    (doto (FileOutputStream. "resources/test3.pdf")
+      (.write byte-arr)
+      (.close))))
 
 (defn -main
   "I don't do a whole lot ... yet."
