6330916dbfa1a317e613220e0b33c9ce5adbc19f
ken44282 <knagai428@gmail.com>
2022-06-25 23:22:08 +0900

    mod

--- test/pdfshield/core_test.clj
+++ test/pdfshield/core_test.clj
@@ -4,65 +4,69 @@
             [pdfshield.core :refer :all])
   (:import [java.io FileInputStream FileOutputStream]))
 
-(def fout (io/as-file "resources/result.pdf"))
-
 (deftest test-set-text
   (testing "test set-text"
-    (when (.exists fout) (.delete fout))
     (let [fis (FileInputStream. "resources/test.pdf")
-          byte-arr (set-text fis 1 20 100 200 "テストテスト")]
+          byte-arr (set-text fis 1 20 100 200 "テストテスト")
+          fout (io/as-file "resources/result-set-text.pdf")]
+      (when (.exists fout) (.delete fout))
       (doto (FileOutputStream. fout)
         (.write byte-arr)
-        (.close)))
-    (is (.exists fout))))
+        (.close))
+      (is (.exists fout)))))
 
 (deftest test-set-image
   (testing "test set-image"
-    (when (.exists fout) (.delete fout))
     (let [fis (FileInputStream. "resources/test.pdf")
           img-ins (FileInputStream. "resources/test.png")
-          byte-arr (set-image fis img-ins 1 (float 20) (float 30) (float 100) (float 200))]
+          byte-arr (set-image fis img-ins 1 (float 20) (float 30) (float 100) (float 200))
+          fout (io/as-file "resources/result-set-image.pdf")]
+      (when (.exists fout) (.delete fout))
       (doto (FileOutputStream. fout)
         (.write byte-arr)
-        (.close)))
-    (is (.exists fout))))
+        (.close))
+      (is (.exists fout)))))
 
 (deftest test-split-page
   (testing "test split-page"
-    (when (.exists fout) (.delete fout))
     (let [fis (FileInputStream. "resources/test.pdf")
-          byte-arr (split-page fis 2 4)]
+          byte-arr (split-page fis 2 4)
+          fout (io/as-file "resources/result-split-page.pdf")]
+      (when (.exists fout) (.delete fout))
       (doto (FileOutputStream. fout)
         (.write byte-arr)
-        (.close)))
-    (is (.exists fout))))
+        (.close))
+      (is (.exists fout)))))
 
 (deftest test-merge-documents
   (testing "test merge-documents"
-    (when (.exists fout) (.delete fout))
     (let [byte-arr (merge-documents (FileInputStream. "resources/aaa.pdf")
-                                    (FileInputStream. "resources/bbb.pdf"))]
+                                    (FileInputStream. "resources/bbb.pdf"))
+          fout (io/as-file "resources/result-merge-documents.pdf")]
+      (when (.exists fout) (.delete fout))
       (doto (FileOutputStream. fout)
         (.write byte-arr)
-        (.close)))
-    (is (.exists fout))))
+        (.close))
+      (is (.exists fout)))))
 
 (deftest test-remove-page
   (testing "test remove-page"
-    (when (.exists fout) (.delete fout))
     (let [fis (FileInputStream. "resources/test.pdf")
-          byte-arr (remove-page fis 2 4)]
+          byte-arr (remove-page fis 2 4)
+          fout (io/as-file "resources/result-remove-page.pdf")]
+      (when (.exists fout) (.delete fout))
       (doto (FileOutputStream. fout)
         (.write byte-arr)
-        (.close)))
-    (is (.exists fout))))
+        (.close))
+      (is (.exists fout)))))
 
 (deftest test-encrypt-page
   (testing "test encrypt-page"
-    (when (.exists fout) (.delete fout))
     (let [fis (FileInputStream. "resources/test.pdf")
-          byte-arr (encrypt-page fis "pass1" "pass2")]
+          byte-arr (encrypt-page fis "pass1" "pass2")
+          fout (io/as-file "resources/result-remove-page.pdf")]
+      (when (.exists fout) (.delete fout))
       (doto (FileOutputStream. fout)
         (.write byte-arr)
-        (.close)))
-    (is (.exists fout))))
+        (.close))
+      (is (.exists fout)))))
